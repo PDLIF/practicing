bc4d7585d48061358625ae9a001401fec5ad767f
astuarykig <akvsevolod@icloud.com>
2025-01-25 19:43:21 +0500

    more refactoring

--- different/scrimba-space-tourism-website-main/tabs.js
+++ different/scrimba-space-tourism-website-main/tabs.js
@@ -14,24 +14,22 @@ function changeTabFocus(e) {
 
     if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
         tabs[tabFocus].setAttribute("tabindex", -1);
-    }
-    
-    if (e.keyCode === keydownLeft) {
-        tabFocus--;
-        if (tabFocus < 0) {
-            tabFocus = tabs.length - 1;
-        }
-    }
 
-    if (e.keyCode === keydownRight) {
-        tabFocus++;
-        if (tabFocus >= tabs.length) {
-            tabFocus = 0;
+        if (e.keyCode === keydownLeft) {
+            tabFocus--;
+            if (tabFocus < 0) {
+                tabFocus = tabs.length - 1;
+            }
+        } else if (e.keyCode === keydownRight) {
+            tabFocus++;
+            if (tabFocus >= tabs.length) {
+                tabFocus = 0;
+            }
         }
-    }
 
-    tabs[tabFocus].setAttribute('tabindex', 0);
-    tabs[tabFocus].focus();
+        tabs[tabFocus].setAttribute('tabindex', 0);
+        tabs[tabFocus].focus();
+    }
 }
 
 function changeTabPanel(e) {
@@ -45,15 +43,19 @@ function changeTabPanel(e) {
     tabList.querySelector('[aria-selected="true"]').setAttribute('aria-selected', false);
     targetTab.setAttribute('aria-selected', true);
 
-    mainContainer
-        .querySelectorAll('[role=tabpanel]')
-        .forEach((article) => {article.setAttribute('hidden', true)});
+    hideContent(mainContainer, '[role="tabpanel"]');
+    showContent(mainContainer, `#${targetPanel}`);
 
-    mainContainer.querySelector(`#${targetPanel}`).removeAttribute('hidden');
+    hideContent(mainContainer, 'picture');
+    showContent(mainContainer, `#${targetImage}`);
+}
 
-    mainContainer
-        .querySelectorAll('picture')
-        .forEach((picture) => {picture.setAttribute('hidden', true)});
+function hideContent(parent, contentSelector) {
+    parent
+        .querySelectorAll(contentSelector)
+        .forEach((panel) => panel.setAttribute('hidden', true));
+}
 
-    mainContainer.querySelector(`#${targetImage}`).removeAttribute('hidden');
+function showContent(parent, contentSelector) {
+    parent.querySelector(contentSelector).removeAttribute('hidden');
 }
